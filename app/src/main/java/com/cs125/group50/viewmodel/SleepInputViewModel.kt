7b01a006a4d098688666b980a46e074a2bc49fa8
mengw15 <125719918+mengw15@users.noreply.github.com>
2024-03-02 21:49:05 -0800

    connect user input sleep info to database

--- app/src/main/java/com/cs125/group50/viewmodel/SleepInputViewModel.kt
+++ app/src/main/java/com/cs125/group50/viewmodel/SleepInputViewModel.kt
@@ -1,12 +1,36 @@
 package com.cs125.group50.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+import java.time.LocalDate
 import java.time.LocalTime
 
 class SleepInputViewModel : ViewModel() {
 
-    fun showSleepData(startTime: LocalTime, endTime: LocalTime) {
-        // 将来添加将数据保存到数据库的逻辑
-        println("Start: $startTime, End: $endTime")
+
+    private val db = Firebase.firestore
+
+    fun saveActivityInfo(userId: String, startTime: LocalTime, endTime: LocalTime, selectedDate: LocalDate) {
+        val sleepInfo = hashMapOf(
+            "date" to selectedDate.toString(),
+            "startTime" to startTime.toString(),
+            "endTime" to endTime.toString()
+        )
+
+        viewModelScope.launch {
+            db.collection("users").document(userId).collection("sleep").add(sleepInfo)
+                .addOnSuccessListener { documentReference ->
+                    Log.d("Firestore", "DocumentSnapshot written with ID: ${documentReference.id}")
+                    // 处理成功保存数据后的逻辑，如更新UI或返回上一页面
+                }
+                .addOnFailureListener { e ->
+                    Log.w("Firestore", "Error adding document", e)
+                    // 处理错误，如显示错误信息
+                }
+        }
     }
 }
