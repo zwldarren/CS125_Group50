eae28817e34399ab3a470c11b546204b7810d80c
Darren Zeng <wenliz@uci.edu>
2024-03-07 21:41:33 -0800

    upgrade gradle and plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,14 @@ plugins {
     id("com.google.gms.google-services")
     id("org.jetbrains.kotlin.android")
 }
-
+fun loadProperties(): Properties {
+    val properties = Properties()
+    val propFile = file("local.properties")
+    if (propFile.exists()) {
+        propFile.inputStream().use { properties.load(it) }
+    }
+    return properties
+}
 android {
     namespace = "com.cs125.group50"
     compileSdk = 34
@@ -27,14 +34,7 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
-        fun loadProperties(): Properties {
-            val properties = Properties()
-            val propFile = file("local.properties")
-            if (propFile.exists()) {
-                propFile.inputStream().use { properties.load(it) }
-            }
-            return properties
-        }
+
         buildConfigField(
             "String",
             "GOOGLE_SIGN_IN_CLIENT_ID",
@@ -87,27 +87,20 @@ chaquopy {
         getByName("py311") { version = "3.11" }
     }
     defaultConfig {
-        pip {
-            "firebase-admin"
-        }
+        buildPython("\"${loadProperties()["PythonPath"]}\"")
     }
 }
 
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0")
-    implementation("androidx.compose.material3:material3-adaptive:1.0.0-alpha06")
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.0.0-alpha03")
     implementation("androidx.health.connect:connect-client:1.0.0-alpha11")
     implementation("androidx.credentials:credentials:1.3.0-alpha01")
     implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
     implementation("com.google.firebase:firebase-auth-ktx")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("androidx.health.connect:connect-client:1.1.0-alpha07")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
-    implementation("androidx.compose.material3:material3-android:1.2.0")
+    implementation("androidx.compose.material3:material3-android:1.2.1")
     implementation("androidx.benchmark:benchmark-common:1.2.3")
     val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
     implementation(composeBom)
@@ -117,31 +110,29 @@ dependencies {
     implementation("com.firebaseui:firebase-ui-firestore:8.0.2")
     implementation("com.firebaseui:firebase-ui-auth:8.0.2")
     implementation("com.firebaseui:firebase-ui-storage:8.0.2")
-    implementation(platform("com.google.firebase:firebase-bom:32.7.1"))
+    implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-analytics")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.01"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation("androidx.compose.ui:ui:1.6.2")
-    implementation("androidx.compose.material3:material3:1.2.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.2")
-    implementation("androidx.compose.runtime:runtime-livedata:1.6.2")
+    implementation("androidx.compose.ui:ui:1.6.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.3")
+    implementation("androidx.compose.runtime:runtime-livedata:1.6.3")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.compose.material:material:1.6.1")
+    implementation("androidx.compose.material:material:1.6.3")
 }
